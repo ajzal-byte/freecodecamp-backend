b527d1f331ca7bdc68c251c06af30bb5c26bcbf5
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 10:23:12 +0530

    File Metadata Microservice added

---
+++ File Metadata Microservice/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require('multer');
+const upload = multer()
+
+var app = express();
+app.use(express.json());
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
